930d5058c997f57e82c7e323a3e020243074e585
GNU Shim <geunwoo.j.shim@gmail.com>
2024-02-19 03:09:34 +0900

    Update create-fhs-volume.sh

--- create-fhs-volume.sh
+++ create-fhs-volume.sh
@@ -1,22 +1,7 @@
 #!/bin/sh
-docker volume create var 
-docker volume create usr
-docker volume create tmp
-docker volume create sys
-docker volume create srv
-docker volume create sbin
-docker volume create run
-docker volume create root
-docker volume create proc
-docker volume create opt
-docker volume create mnt
-docker volume create media
-docker volume create libx32
-docker volume create lib64
-docker volume create lib32
-docker volume create lib
-docker volume create home
-docker volume create etc
-docker volume create dev
-docker volume create boot
+docker volume create var; docker volume create usr; docker volume create tmp; docker volume create sys
+docker volume create srv; docker volume create sbin; docker volume create run; docker volume create root
+docker volume create proc; docker volume create opt; docker volume create mnt; docker volume create media
+docker volume create libx32; docker volume create lib64; docker volume create lib32; docker volume create lib
+docker volume create home; docker volume create etc; docker volume create dev; docker volume create boot
 docker volume create bin
