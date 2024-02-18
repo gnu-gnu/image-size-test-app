d11e160293ab736806f459a63229aa58ad10e2d5
GNU Shim <geunwoo.j.shim@gmail.com>
2024-02-19 03:08:06 +0900

    Create run-distroless-with-volume.sh

---
+++ run-distroless-with-volume.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+docker run \
+-v var:/var \
+-v usr:/usr \
+-v tmp:/tmp \
+-v sys:/sys \
+-v srv:/srv \
+-v sbin:/sbin \
+-v run:/run \
+-v root:/root \
+-v opt:/opt \
+-v mnt:/mnt \
+-v media:/media \
+-v libx32:/libx32 \
+-v lib64:/lib64 \
+-v lib32:/lib32 \
+-v lib:/lib \
+-v home:/home \
+-v etc:/etc \
+-v boot:/boot \
+-v bin:/bin \
+ -d  --name app image-size-test:distroless-based
